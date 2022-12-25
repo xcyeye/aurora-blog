4a3ee91f428c46006d9779edf6e387bbed0eb744
xcyeye <2291308094@qq.com>
2022-12-26 00:06:19 +0800

    feat(components): finish email module

--- src/views/message-center/email-manage/index.vue
+++ src/views/message-center/email-manage/index.vue
@@ -1,11 +1,17 @@
 <template>
-	<div></div>
+	<div>
+		<user-list/>
+		<user-modify/>
+	</div>
 </template>
 
 <script lang="ts" setup>
 import {defineComponent} from "vue";
+import UserList from './user-list/index.vue'
+import UserModify from './user-modify/index.vue'
 
 defineComponent({name: 'index'});
+
 </script>
 
 <style scoped>
