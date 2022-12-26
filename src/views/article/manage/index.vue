4a88720028d8ecde8c77b57712d2f752a1ca21bf
xcyeye <2291308094@qq.com>
2022-12-27 00:17:09 +0800

    feat(components): finish article list

--- src/views/article/manage/index.vue
+++ src/views/article/manage/index.vue
@@ -1,11 +1,17 @@
 <template>
-	<div></div>
+	<div>
+		<article-list/>
+		<article-modify/>
+	</div>
 </template>
 
 <script lang="ts" setup>
 import {defineComponent} from "vue";
+import ArticleList from './article-list/index.vue'
+import ArticleModify from './article-modify/index.vue'
 
 defineComponent({name: 'index'});
+
 </script>
 
 <style scoped>
