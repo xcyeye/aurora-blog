d59456141709172787bfa7f9c265995ac193e99f
xcyeye <2291308094@qq.com>
2023-02-01 00:55:54 +0800

    fix(pageWeb): 解决登录过期bug

--- auroraPageWeb/src/service/request/helpers.ts
+++ auroraPageWeb/src/service/request/helpers.ts
@@ -5,5 +5,11 @@ import type { AxiosRequestConfig } from 'axios';
  * @param axiosConfig - token失效时的请求配置
  */
 export async function handleRefreshToken(axiosConfig: AxiosRequestConfig) {
+  // TODO 处理token过期，目前先清楚缓存
+  localStorage.clear()
+  window.$message?.error('登录信息已过期，请重新登录')
+  if (window) {
+    window.location.reload()
+  }
   return null;
 }
