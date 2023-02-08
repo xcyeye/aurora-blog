25c7a924548cf09da321c8959f61a449a6df31d9
xcyeye <2291308094@qq.com>
2023-02-08 23:01:31 +0800

    fix(pageweb): fix some bug

--- shell/run-backend.sh
+++ shell/run-backend.sh
@@ -1,10 +1,10 @@
 # !/bin/bash
 cd /opt/aurora-blog-system-web/backend
 
-java -jar article-boot-1.0-SNAPSHOT.jar -Xms200m -Xmx300m
-java -jar aurora-admin-1.0-SNAPSHOT.jar -Xms200m -Xmx300m
-java -jar auth-server-boot-1.0-SNAPSHOT.jar -Xms200m -Xmx250m
-java -jar comment-boot-1.0-SNAPSHOT.jar -Xms200m -Xmx250m
-java -jar file-boot-1.0-SNAPSHOT.jar -Xms200m -Xmx300m
-java -jar gateway-boot-1.0-SNAPSHOT.jar -Xms200m -Xmx300m
-java -jar message-boot-1.0-SNAPSHOT.jar -Xms200m -Xmx300m
+java -jar article-boot-1.0-SNAPSHOT.jar --spring.profiles.active=prod -Xms200m -Xmx300m
+java -jar admin-boot-1.0-SNAPSHOT.jar --spring.profiles.active=prod -Xms200m -Xmx300m
+java -jar auth-server-boot-1.0-SNAPSHOT.jar --spring.profiles.active=prod -Xms200m -Xmx250m
+java -jar comment-boot-1.0-SNAPSHOT.jar --spring.profiles.active=prod -Xms200m -Xmx250m
+java -jar file-boot-1.0-SNAPSHOT.jar --spring.profiles.active=prod -Xms200m -Xmx300m
+java -jar gateway-boot-1.0-SNAPSHOT.jar --spring.profiles.active=prod -Xms200m -Xmx300m
+java -jar message-boot-1.0-SNAPSHOT.jar --spring.profiles.active=prod -Xms200m -Xmx300m
