da7369aa7857f4d22a2c5b251eab91c57ccda19d
xcyeye <2291308094@qq.com>
2023-02-08 21:45:45 +0800

    build(shell): update shell

--- shell/build_adminWeb.sh
+++ shell/build_adminWeb.sh
@@ -1,4 +1,10 @@
 # !/bin/bash
+
+mkdir -p /opt/aurora-blog-system-web/pageWeb
+mkdir -p /opt/aurora-blog-system-web/adminWeb
+mkdir -p /opt/aurora-blog-system-web/backend
 echo "正在打包adminWeb模块"
 cd ../auroraAdminWeb
 pnpm build-aurora
+echo "正在移动adminWeb"
+mv -f /opt/aurora-blog-system/auroraAdminWeb/dist /opt/aurora-blog-system-web/adminWeb
