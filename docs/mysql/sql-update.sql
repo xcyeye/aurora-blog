c291170ac3d6f3bda0a80b1bd8299f9d53fd1989
xcyeye <2291308094@qq.com>
2023-02-09 23:12:31 +0800

    docs(): update sql-update

--- docs/mysql/sql-update.sql
+++ docs/mysql/sql-update.sql
@@ -5,3 +5,7 @@ alter table au_category
 # 2023-2-9 xcye 取消标签名称的唯一性
 alter table au_tag
     drop key unique_title_index;
+
+# 2023-2-9 xcye 
+alter table au_login_info
+    modify login_ip varchar(128) not null comment '登录ip地址';
