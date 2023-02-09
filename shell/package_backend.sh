fb35bbf0fd5766e2f4b1a27cc9e55c39c6559927
xcyeye <2291308094@qq.com>
2023-02-09 12:30:32 +0800

    fix(shell): fix mv cannot move

--- shell/package_backend.sh
+++ shell/package_backend.sh
@@ -5,28 +5,28 @@ mkdir -p /opt/aurora-blog-system-web/backend
 
 cd ../auroraBackend/aurora-modules/admin-boot
 mvn clean package
-mv -f /opt/aurora-blog-system/auroraBackend/aurora-modules/admin-boot/target/admin-boot-1.0-SNAPSHOT.jar /opt/aurora-blog-system-web/backend
+rsync -a /opt/aurora-blog-system/auroraBackend/aurora-modules/admin-boot/target/admin-boot-1.0-SNAPSHOT.jar /opt/aurora-blog-system-web/backend
 
 cd ../article-boot
 mvn clean package
-mv -f /opt/aurora-blog-system/auroraBackend/aurora-modules/article-boot/target/article-boot-1.0-SNAPSHOT.jar /opt/aurora-blog-system-web/backend
+rsync -a /opt/aurora-blog-system/auroraBackend/aurora-modules/article-boot/target/article-boot-1.0-SNAPSHOT.jar /opt/aurora-blog-system-web/backend
 
 cd ../gateway-boot
 mvn clean package
-mv -f /opt/aurora-blog-system/auroraBackend/aurora-modules/gateway-boot/target/gateway-boot-1.0-SNAPSHOT.jar /opt/aurora-blog-system-web/backend
+rsync -a /opt/aurora-blog-system/auroraBackend/aurora-modules/gateway-boot/target/gateway-boot-1.0-SNAPSHOT.jar /opt/aurora-blog-system-web/backend
 
 cd ../comment-boot
 mvn clean package
-mv -f /opt/aurora-blog-system/auroraBackend/aurora-modules/comment-boot/target/comment-boot-1.0-SNAPSHOT.jar /opt/aurora-blog-system-web/backend
+rsync -a /opt/aurora-blog-system/auroraBackend/aurora-modules/comment-boot/target/comment-boot-1.0-SNAPSHOT.jar /opt/aurora-blog-system-web/backend
 
 cd ../file-boot
 mvn clean package
-mv -f /opt/aurora-blog-system/auroraBackend/aurora-modules/file-boot/target/file-boot-1.0-SNAPSHOT.jar /opt/aurora-blog-system-web/backend
+rsync -a /opt/aurora-blog-system/auroraBackend/aurora-modules/file-boot/target/file-boot-1.0-SNAPSHOT.jar /opt/aurora-blog-system-web/backend
 
 cd ../message-boot
 mvn clean package
-mv -f /opt/aurora-blog-system/auroraBackend/aurora-modules/message-boot/target/message-boot-1.0-SNAPSHOT.jar /opt/aurora-blog-system-web/backend
+rsync -a /opt/aurora-blog-system/auroraBackend/aurora-modules/message-boot/target/message-boot-1.0-SNAPSHOT.jar /opt/aurora-blog-system-web/backend
 
 cd ../auth-server-boot
 mvn clean package
-mv -f /opt/aurora-blog-system/auroraBackend/aurora-modules/auth-server-boot/target/auth-server-boot-1.0-SNAPSHOT.jar /opt/aurora-blog-system-web/backend
+rsync -a /opt/aurora-blog-system/auroraBackend/aurora-modules/auth-server-boot/target/auth-server-boot-1.0-SNAPSHOT.jar /opt/aurora-blog-system-web/backend
