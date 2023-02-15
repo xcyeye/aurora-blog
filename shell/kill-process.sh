2b33a40eb5f82ef939ea2c4999cb612d860a18f1
xcyeye <2291308094@qq.com>
2023-02-15 16:47:30 +0800

    docs(shell):  增加杀死线程脚本

---
+++ shell/kill-process.sh
@@ -0,0 +1,16 @@
+# !/bin/bash
+boot_names=(article-boot admin-boot auth-server comment-boot file-boot gateway-boot)
+version_name=1.0-SNAPSHOT.jar
+for element in ${boot_names[@]}
+do
+  jar_name=${element}-${version_name}
+  echo "正在杀死 ${jar_name} 进程"
+  process_pid=`ps -elf |grep message|grep -v grep |head -n 1 |awk '{printf $4}'`
+  echo ${process_pid}
+
+  if [[ -n $process_pid ]];
+  then
+    echo "${jar_name} 的进程pid为 ${process_pid}"
+    kill -9 ${process_pid}
+  fi
+done
