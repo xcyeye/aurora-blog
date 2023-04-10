d1371d3f12b89a72ce0e1a5be2acd2deed06706e
xcyeye <2291308094@qq.com>
2023-04-10 11:33:54 +0800

    docs(shell): update run shell

--- shell/run-backend.sh
+++ shell/run-backend.sh
@@ -1,10 +1,10 @@
 # !/bin/bash
 cd /opt/aurora-blog-system-web/backend
 
-nohup java -jar article-boot-1.0-SNAPSHOT.jar --spring.profiles.active=prod -Xms150m -Xmx250m &
-nohup java -jar admin-boot-1.0-SNAPSHOT.jar --spring.profiles.active=prod -Xms150m -Xmx250m &
-nohup java -jar auth-server-boot-1.0-SNAPSHOT.jar --spring.profiles.active=prod -Xms150m -Xmx200m &
-nohup java -jar comment-boot-1.0-SNAPSHOT.jar --spring.profiles.active=prod -Xms150m -Xmx200m &
-nohup java -jar file-boot-1.0-SNAPSHOT.jar --spring.profiles.active=prod -Xms150m -Xmx250m &
-nohup java -jar gateway-boot-1.0-SNAPSHOT.jar --spring.profiles.active=prod -Xms200m -Xmx300m &
-nohup java -jar message-boot-1.0-SNAPSHOT.jar --spring.profiles.active=prod -Xms150m -Xmx250m &
+nohup java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:50001 admin-boot-1.0-SNAPSHOT.jar --spring.profiles.active=prod -Xms150m -Xmx250m &
+nohup java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:50002 article-boot-1.0-SNAPSHOT.jar --spring.profiles.active=prod -Xms150m -Xmx250m &
+nohup java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:50003 auth-server-boot-1.0-SNAPSHOT.jar --spring.profiles.active=prod -Xms150m -Xmx200m &
+nohup java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:50004 comment-boot-1.0-SNAPSHOT.jar --spring.profiles.active=prod -Xms150m -Xmx200m &
+nohup java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:50005 file-boot-1.0-SNAPSHOT.jar --spring.profiles.active=prod -Xms150m -Xmx250m &
+nohup java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:50006 gateway-boot-1.0-SNAPSHOT.jar --spring.profiles.active=prod -Xms200m -Xmx300m &
+nohup java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:50007 message-boot-1.0-SNAPSHOT.jar --spring.profiles.active=prod -Xms150m -Xmx250m &
